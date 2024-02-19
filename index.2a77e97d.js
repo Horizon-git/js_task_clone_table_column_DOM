5f306a5a2ae90daaa121b94ed53b07b4c0b3c20b
Horizon-git <apbreloaded947@gmail.com>
2024-02-19 19:03:15 +0200

    make build

---
+++ index.2a77e97d.js
@@ -0,0 +1,2 @@
+var r=document.querySelectorAll("tr"),e=!0,l=!1,t=void 0;try{for(var n,o=r[Symbol.iterator]();!(e=(n=o.next()).done);e=!0){var a=n.value,i=a.children[1].cloneNode(!0);a.lastElementChild.before(i)}}catch(r){l=!0,t=r}finally{try{e||null==o.return||o.return()}finally{if(l)throw t}}
+//# sourceMappingURL=index.2a77e97d.js.map
